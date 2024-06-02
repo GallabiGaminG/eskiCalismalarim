8268fb2466a2ff875fd6ed86f983083a5228e42a
GallabiGaminG <bersan_guzel3306@hotmail.com>
2024-06-02 16:47:30 +0300

    Eski çalışmalar
    Tüm çalışmalar tek dizine toplandı ve düzenlenmiş oldu içerkleri kontrol edilecek.

---
+++ Unity/CarRace/Assets/Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,33 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    public GameObject inventory;
+    public GameObject inventoryEkran;
+    //GameObject inventory = GameObject.Find("Inventory");
+    ManaBar manaBar;
+    //Inventory inventory;
+    //var temp = GameObject.Find("Inventory");
+    //public var a = GameObject.Invetory;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory = Inventory.instance;
+        inventory.onItemChangedCallBack += UpdateUI;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void UpdateUI()
+    {
+        Debug.Log("UPDATING UI");
+    }
+
+}
